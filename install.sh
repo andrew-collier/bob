2b8426854f6df2a71ea25774ec64aeb8c9b4a773
andrew-collier <100000174+andrew-collier@users.noreply.github.com>
2022-09-27 14:09:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 [ ! $(which zsh) ] && echo "Missing dependency \"zsh\"" && exit 1
 [ ! $(which less) ] && echo "Missing dependency \"less\"" && exit 1
+[ ! $(which dos2unix) ] && echo "Missing dependency \"dos2unix\"" && exit 1
 
 make all
-install -Dm 755 "./bill" "/usr/local/bin/kjv"
-install -Dm 755 "./bob" "/usr/local/bin/kjv"
-install -Dm 444 "./bob.1" "/usr/local/man/man1/kjv.1"
+
+install -Dm 755 "./bill" "/usr/local/bin/bill"
+install -Dm 755 "./bob" "/usr/local/bin/bob"
+install -Dm 444 "./bob.1" "/usr/local/man/man1/bob.1"
 mandb -q
